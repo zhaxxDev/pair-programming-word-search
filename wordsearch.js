aee8acccdc028a6278ae510ed384c4cd5b3e22f5
Z Xian <alex.xian@hotmail.com>
2020-09-24 01:10:25 +0000

    done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
 const wordSearch = (letters, word) => { 
+    
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        // console.log('horizontalJoin:', horizontalJoin);
+        if (l.includes(word)) {
+            return true;
+        }
     }
+​
+    
+    let arr = [];
+    for (let i = 0; i < letters.length - 1; i++) {
+        let wordx = [];
+        for (let j = 0; j < letters.length; j++){
+            wordx.push(letters[j][i])
+            // let wordy = wordx.join('');
+            if (wordx.join("").includes(word)) {
+                return true;
+            }
+        }
+        // console.log(wordx.join(""))
+    }
+    return false;
 }
-
-module.exports = wordSearch
+​
+//made by Z Xian, Tom Adam, Mohammed Abdulbaqi
+module.exports = wordSearch 
